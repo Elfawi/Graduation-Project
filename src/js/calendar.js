07ba73fc1bbdfc9304a37bec6faa52c66d6b1351
Mohamed Elfawi <elfawi123@gmail.com>
2025-02-13 02:54:31 +0200

    calendar feature

---
+++ src/js/calendar.js
@@ -0,0 +1,107 @@
+const event = {
+  events: [
+    {
+      id: "a",
+      title: "my event",
+      start: "2018-09-01",
+    },
+  ],
+  addEvent: function (event) {
+    this.events.push(event);
+  },
+};
+const eventModal = document.querySelector(".event-modal");
+const overlay = document.querySelector(".overlay");
+document.addEventListener("DOMContentLoaded", function () {
+  const calendarEl = document.getElementById("calendar");
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    headerToolbar: {
+      left: "prev,next today",
+      center: "title",
+      right: "dayGridMonth,timeGridWeek,timeGridDay",
+      center: "addEventButton",
+    },
+    events: [
+      {
+        id: "a",
+        title: "my event",
+        start: "2025-02-01",
+        end: "2025-02-04",
+      },
+      {
+        id: "b",
+        title: "my event 2",
+        start: "2025-02-02",
+      },
+    ],
+    views: {
+      dayGridMonth: {
+        // name of view
+        // titleFormat: { year: "2-digit", month: "2-digit", day: "2-digit" },
+        // other view-specific options here
+      },
+    },
+    // initialView: "dayGridMonth",
+    // eventColor: "#ff0000",
+    dateClick: function (info) {
+      // alert("a day has been clicked!");
+      console.log("clicked on " + info.dateStr); // geting the clicked date
+      // info.dayEl.style.backgroundColor = "#8b30cd";
+      // console.log(info.resource);
+    },
+    customButtons: {
+      addEventButton: {
+        text: "add event...",
+        click: function () {
+          // var dateStr = prompt("Enter a date in YYYY-MM-DD format");
+          // var date = new Date(dateStr + "T00:00:00"); // will be in local time
+          // console.log(date.valueOf());
+          eventModal.classList.remove("modal-hidden");
+          eventModal.classList.add("modal-show");
+          overlay.style.display = "block";
+          //   /// isNaN IS A FUNCTION THAT RETURN TRUE IF CONVERTED TO INT SUCCESSFULY
+          // if (!isNaN(date.valueOf())) {
+          //   // console.log(isNaN(date.valueOf));
+          //   // valid?
+          //   calendar.addEvent({
+          //     // title: "dynamic event",
+          //     title: prompt("Event title"),
+          //     start: date,
+          //     allDay: true,
+          //   });
+          //   calendar.addEvent(calendar.events[1]);
+          //   alert("Great. Now, update your database...");
+          // } else {
+          //   alert("Invalid date.");
+          // }
+        },
+      },
+    },
+  });
+  calendar.render();
+});
+
+document.addEventListener("click", function (e) {
+  // console.log(
+  //   // e.target.closest(".event-modal").classList.contains("event-modal")
+  // );
+  // const modal = e.target
+  //   .closest(".event-modal")
+  //   .classList.contains("modal-show");
+  // console.log(modal);
+  if (
+    e.target.closest(".event-modal") !== eventModal &&
+    e.target.type !== "button"
+  ) {
+    eventModal.classList.remove("modal-show");
+    eventModal.classList.add("modal-hidden");
+    overlay.style.display = "none";
+  }
+});
+// calendar.on("dateClick", function (info) {
+//   console.log("clicked on " + info.dateStr); // geting the clicked date
+//   console.log(info);
+//   // info.dayEl.querySelector(".fc-daygrid-day-events").textContent = "hello";
+// });
+////////////////////////// Calendar Events
+// calendar.addEvent(event[prompt("enter date")]);
